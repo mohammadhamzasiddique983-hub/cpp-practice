c18ec76b82307d1cf0fbc3d229ddcbf4bc5d746f
Mohammad Hamza <mohammadhamzasiddique983@gmail.com>
2026-06-18 17:05:37 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -5,7 +5,7 @@ int Binary_search(int arr[], int size,int element){
     low=0;
     high=size-1;
     while(low<=high){
-        mid=(low+high)/2;
+        mid=low(high-low)/2;
         if(arr[mid]==element){
             return mid;
         }
@@ -16,7 +16,7 @@ int Binary_search(int arr[], int size,int element){
             high=mid-1;
         }
     }
-    
+   return -1; 
 }
 int main(){
     int arr[]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
