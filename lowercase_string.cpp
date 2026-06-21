4f6c52d1833d2d7df81f7d370c76e89913bd2e5d
Mohammad Hamza <mohammadhamzasiddique983@gmail.com>
2026-06-21 13:50:38 +0530

    Added C++ and DSA practice programs

---
+++ lowercase_string.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    string str;
+
+    cout<<"Enter a string : ";
+    cin>>str;
+    for(int i=0; i<str.length(); i++){
+        if(str[i]>='A' && str[i]<='Z')
+        str[i]=str[i]+32;
+    }
+    cout<<str;
+    return 0;
+}
