f90488637bceae9a8dd922c0b3fe6a46d400a580
Mohammad Hamza <mohammadhamzasiddique983@gmail.com>
2026-06-22 23:57:34 +0530

    Added C++ and DSA practice programs

---
+++ Selection_Sort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void selection_sort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        int smallestindex=i ; //unsorted part starting
+        for(int j=i+1; j<n; j++){
+            if(arr[j]< arr[smallestindex]){
+                smallestindex=j;
+            }
+        }
+        swap(arr[i], arr[smallestindex]);
+    }
+}
+void printarray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main() {
+int n=5;
+int arr[]= {4, 1, 5, 2, 3};
+selection_sort(arr, n);
+printarray(arr,n);
+return 0;
+}
